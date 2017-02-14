6e0f6b17f64cf95f7e1d6b7a6efada16db554739
heber <heberbjones@gmail.com>
2017-02-13 20:14:13 -0700

    link the logo, and fixed header

--- mainplace/main.js
+++ mainplace/main.js
@@ -1,6 +1,7 @@
 var colbtn = document.getElementById("colbtn");
 var body = document.getElementById("body");
 var lefter = document.getElementById("lefter");
+var header = document.getElementById("header");
 var colstat = {
 	
 	seen:"yes"
@@ -16,12 +17,14 @@ function leftermovement () {
 		lefter.style.left = "-250px";
 		body.style.marginLeft = "0";
 		colbtn.setAttribute("class","fa fa-angle-right");
+		header.style.left = "0";
 		colstat.seen = "no";
 	}
 	
 	else if (colstat.seen === "no") {
 		lefter.style.left = "0px";
 		body.style.marginLeft = "250";
+		header.style.left = "250px";
 		colbtn.setAttribute("class","fa fa-angle-left");
 		colstat.seen = "yes";
 	}
